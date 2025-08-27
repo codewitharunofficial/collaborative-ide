df09b66ee6ddcfdc1592dc39fc488311cbda17b6
Arun <arunonwork3249@gmail.com>
2025-08-27 16:09:56 +0530

    Updated BASE URL

--- src/utilities/SocketServices.js
+++ src/utilities/SocketServices.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const appUrl = process.env.NEXT_APP_PUBLIC_URL || "http://localhost:1234";
+const appUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:1234";
 
 const socket = io(appUrl);
 
