3e82cc013f52b7fbc01af247a9cec76ddcd0dd79
Arun <arunonwork3249@gmail.com>
2025-08-27 16:57:35 +0530

    Replaced <a> with <Link>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <nav className="bg-[#1e1e1e] text-white px-6 py-3 flex items-center justify-between shadow-md">
             <h1 className="text-lg font-bold">⚡ Collab Code</h1>
             <div className="space-x-4">
-                <a href="/" className="hover:text-gray-300">Home</a>
-                <a href="/about" className="hover:text-gray-300">About</a>
-                <a href="https://github.com/codewitharunofficial/collaborative-ide" target="_blank" className="hover:text-gray-300">GitHub</a>
+                <Link href="/" className="hover:text-gray-300">Home</Link>
+                <Link href="/about" className="hover:text-gray-300">About</Link>
+                <Link href="https://github.com/codewitharunofficial/collaborative-ide" target="_blank" className="hover:text-gray-300">GitHub</Link>
             </div>
         </nav>
     );
